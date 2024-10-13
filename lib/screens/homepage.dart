fd3ab9fb3e60d5d9f384a482deeb0980e5705b88
lshee9008 <9008lucky@naver.com>
2024-10-14 01:04:22 +0900

    version1

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dot_indicator_test/pages/page_1.dart';
+import 'package:flutter_dot_indicator_test/pages/page_2.dart';
+import 'package:flutter_dot_indicator_test/pages/page_3.dart';
+import 'package:flutter_dot_indicator_test/pages/page_4.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _controller = PageController();
+
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            height: 500,
+            child: PageView(
+              controller: _controller,
+              children: [
+                Page1(),
+                Page2(),
+                Page3(),
+                Page4(),
+              ],
+            ),
+          ),
+          SmoothPageIndicator(
+            controller: _controller,
+            count: 4,
+            effect: ExpandingDotsEffect(
+              activeDotColor: Colors.deepPurple,
+              dotColor: Colors.deepPurple.shade100,
+              dotHeight: 30,
+              dotWidth: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
